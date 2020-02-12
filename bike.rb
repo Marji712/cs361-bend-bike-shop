28d0799077a068af44e3fa9030bc17c9e7514917
Marji Symonds <symondsm@oregonstate.edu>
2020-02-12 10:48:27 -0800

    Remove excess code from merge resolution

--- bike.rb
+++ bike.rb
@@ -1,46 +1,4 @@
-<<<<<<< HEAD
-class Bike
 
-  attr_accessor :id, :color, :price, :weight, :rented
-
-  def initialize(id, color, price, rented = false)
-    @id = id
-    @color = color
-    @price = price
-    @rented = rented
-  end
-
-  def rent!
-    self.rented = true
-  end
-end
-
-class Pannier
-
-MAX_CARGO_ITEMS = 10
-
-attr_accessor :contents, :total_capacity, :remaining_capacity
-
-def initialize(contents, total_capacity, remaining_capacity)
-  @contents = []
-  @total_capacity = MAX_CARGO_ITEMS
-  @remaining_capacity = MAX_CARGO_ITEMS
-end
-
-  def add_cargo(item)
-    self.contents << item
-  end
-
-  def remove_cargo(item)
-    self.contents.remove(item)
-  end
-
-  def remaining_capacity
-    MAX_CARGO_ITEMS - self.contents.size
-  end
-end
-
-=======
 # Bike
 require_relative 'luggage'
 
@@ -59,4 +17,4 @@ class Bike
   end
 
 end
->>>>>>> f40810edb0a7530390426b4b46eecca69fa287f2
+
--- main.rb
+++ main.rb
@@ -1,18 +1,6 @@
 !# /usr/bin/env ruby
 
 require_relative 'bike'
-<<<<<<< HEAD
-
-bike = Bike.new(1, :pink, 99.99)
-
-pannier.add_cargo(:apples)
-pannier.add_cargo(:water)
-pannier.add_cargo(:repair_kit)
-
-puts "Space for #{pannier.remaining_capacity} items left."
-
-bike.rent!
-=======
 require_relative 'rental'
 
 items = [:apple, :water, :protein_bar]
@@ -23,4 +11,3 @@ rental = Rental.new(bike)
 
 puts "Total Price: #{rental.price}, Total Weight: #{rental.weight}"
 
->>>>>>> f40810edb0a7530390426b4b46eecca69fa287f2
